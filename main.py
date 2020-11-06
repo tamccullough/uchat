f1b8dfd4929eae55a2d57bfcb6af257c92c09e77
tamccullough <howdytodd@gmail.com>
2020-11-06 15:26:13 -0500

    system works, removing import of cb_main for now

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
 
-import cb_main as cbu
+#import cb_main as cbu
 
 import numpy as np
 import pandas as pd
@@ -11,7 +11,7 @@ import pandas as pd
 
 theme = 'demon'
 chatbot = 'Demonicus'
-title = 'Speak To '+chatbot
+title = 'Talk To '+chatbot
 
 chats = Flask(__name__)
 
@@ -22,7 +22,7 @@ def index():
     print(generated_text['responses'])
 
     return render_template('index.html',
-    generated_text = generated_text, description = description,
+    generated_text = generated_text,
     theme = theme, title = title, chatbot = chatbot)
 
 # generated chats will end up here
@@ -32,7 +32,7 @@ def generate():
     generated_text = cbu.generate_text(start_string=sentence+u'\n',temperature = 0.9)
 
     return render_template('index.html',
-    generated_text = generated_text, description = description,
+    generated_text = generated_text,
     theme = theme, title = title, chatbot = chatbot)
 
 # testing section
